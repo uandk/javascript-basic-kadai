46c1cfcf320d497f92917942141eb13528bc6582
uandk <141488893+uandk@users.noreply.github.com>
2023-10-10 13:09:53 +0900

    bmi.js

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,4 +1,7 @@
 // BMI計算式
-let weight = 68
-let height = 1.7
-console.log( weight / (height * height))
+let weight;
+let height;
+ weight = 68
+ height = 1.7
+let BMI;
+ console.log( weight / (height * height));
